0092c1be89df8ef2d8305cc9da2c9c296f03243a
malinda <malinda@malindas-air>
2024-05-09 13:03:32 +0530

    add dependencies for cucumber

--- TestAutoPro/src/test/java/com/swaglabs/SD/TestRunner.java
+++ TestAutoPro/src/test/java/com/swaglabs/SD/TestRunner.java
@@ -1,5 +1,16 @@
 package com.swaglabs.SD;
 
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "",
+		glue = ""
+		)
+
 public class TestRunner {
 
 }
